1179757663372e784895904df6653eaf45cd88de
LaSauceBBQ <85407801+LaSauceBBQ@users.noreply.github.com>
2023-01-03 10:11:53 +0100

    Add files via upload

---
+++ javascript/bar_script.js
@@ -0,0 +1,26 @@
+function bienvenu() {
+    alert("Bienvenu");
+}
+
+function ajouter(){
+
+    var newTaskForm = document.forms.newTask;
+    console.log(newTaskForm.elements.categorie.value);
+    console.log(newTaskForm.elements.tache.value);
+    
+
+    const newItem = document.createElement('tr');
+    const td1  = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+
+    td1.textContent = newTaskForm.elements.tache.value;
+    td2.textContent = newTaskForm.elements.date.value;
+    td3.textContent = newTaskForm.elements.categorie.value;
+    newItem.append(td1, td2, td3);
+    console.log(newItem);
+    console.log();
+    document.querySelector(".datatable").appendChild(newItem);
+    
+    
+}
